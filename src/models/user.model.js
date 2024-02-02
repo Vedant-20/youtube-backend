087c6d9d7642f208b79b31f7befc311e8f6ecff4
Vedant <vedantwarjurkar@gmail.com>
2024-02-02 13:44:11 +0530

    user schema

---
+++ src/models/user.model.js
@@ -0,0 +1,57 @@
+import { Schema } from "mongoose";
+import mongoose from "mongoose";
+
+
+const userSchema=new Schema(
+    {
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        index:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        
+    },
+    fullname:{
+        type:String,
+        required:true,
+        
+        index:true,
+        trim:true,
+        
+    },
+    avatar:{
+        type:String,
+        required:true,
+        
+    },
+    coverImage:{
+        type:String
+    },
+    watchHistory:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:"Video"
+        }
+    ],
+    password:{
+        type:String,
+        required:[true,'Password is Required']
+    },
+    refreshToken:{
+        type:String
+    }
+},{
+    timestamps:true
+}
+)
+
+export const User=mongoose.model('User',userSchema)
---
+++ src/models/video.model.js
@@ -0,0 +1,13 @@
+import mongoose,{Schema} from "mongoose";
+
+const videoSchema=new Schema(
+    {
+        
+    },
+    {
+        timestamps:true
+    }
+)
+
+
+export const Video=mongoose.model('Video',videoSchema)
