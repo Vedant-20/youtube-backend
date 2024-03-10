74219f0a41116792263a63a28fa731fd3997a474
Vedant <vedantwarjurkar@gmail.com>
2024-03-10 12:34:39 +0530

    playlist controllers addded

--- src/app.js
+++ src/app.js
@@ -27,6 +27,7 @@ import userRouter from './routes/user.routes.js'
 import videoRouter from './routes/video.routes.js'
 import tweetRouter from './routes/tweet.routes.js'
 import subscriptionRouter from './routes/subscription.routes.js'
+import playlistRouter from './routes/playlist.routes.js'
 
 
 //routes declarartion
@@ -34,6 +35,7 @@ app.use('/api/v1/users',userRouter)
 app.use('/api/v1/videos',videoRouter)
 app.use('/api/v1/tweets',tweetRouter)
 app.use('api/v1/subscriptions',subscriptionRouter)
+app.use('api/v1/playlist',playlistRouter)
 
 //https:localhost:8000/api/v1/users/resgiter  ----example
 
