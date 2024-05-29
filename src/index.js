25ddea2868fc73f7c4c1725c07732dc441d5d5d8
Vedant <vedantwarjurkar@gmail.com>
2024-05-29 15:02:24 +0530

    comment out jobs start

--- src/index.js
+++ src/index.js
@@ -1,31 +1,27 @@
 // require('dotenv').config({path:'./env'})
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
 // import mongoose from "mongoose";
 // import {DB_NAME} from './constants'
 import connectDB from "./db/index.js";
-import { app } from './app.js';
-import job from './cron/cron.js';
-
+import { app } from "./app.js";
+import job from "./cron/cron.js";
 
 dotenv.config({
-    path:'./env'
-})
+  path: "./env",
+});
 
-job.start();
-
+// job.start();
 
 connectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 8000 , ()=>{
-        console.log(`Server is running at port : ${process.env.PORT}`)
-    })
-})
-.catch((err)=>{
-    console.log("MongoDb connection failed !!!!",err)
-})
-
-
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDb connection failed !!!!", err);
+  });
 
 /*
 import express from 'express'
